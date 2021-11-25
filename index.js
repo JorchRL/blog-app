50dec3a03e783f1fd657b8cb4240f8fff47a6880
Jorch <jorch.rl@gmail.com>
2021-11-24 20:28:35 -0600

    Finish exercises 4.1 and 4.2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const http = require("http");
 const config = require("./utils/config");
 const logger = require("./utils/logger");
 
+// Blog list app server (exercises 4.1 and 4.2)
 const server = http.createServer(app);
 
 server.listen(config.PORT, () => {
