5f1d0e70f6e08ae5d4ecf4153a0f9219138e0bce
Jorch <jorch.rl@gmail.com>
2021-11-25 11:29:23 -0600

    Finish exercise 4.4

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -1,4 +1,5 @@
 const listHelper = require("../utils/list_helper");
+const blogTestCases = require("./blog_test_cases");
 
 describe("dummy", () => {
   test("dummy returns one", () => {
@@ -10,19 +11,20 @@ describe("dummy", () => {
 });
 
 describe.only("total likes", () => {
-  const listWithOneBlog = [
-    {
-      _id: "123",
-      title: "How to make a million dollars writing tests!",
-      author: "Me :3",
-      url: "www.katitos.art/blogs/123",
-      likes: "10",
-      __v: "0",
-    },
-  ];
+  const { listWithOneBlog, blogs } = blogTestCases;
+
+  test("when list is empty, equals 0", () => {
+    const result = listHelper.totalLikes([]);
+    expect(result).toBe(0);
+  });
 
   test("when the list has just one blog, equals the likes of that", () => {
     const result = listHelper.totalLikes(listWithOneBlog);
     expect(result).toBe(10); // our dummy blog above has 10 likes
+  });
+
+  test("when the list has many blogs, equals the sums of the likes of each", () => {
+    const result = listHelper.totalLikes(blogs);
+    expect(result).toBe(36);
   });
 });
--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,8 +4,19 @@ const dummy = (blogs) => {
 
 const totalLikes = (blogs) => {
   // Should return the sum of likes of all blog posts in the blogs array
-  return 0;
+
+  return blogs.length === 0
+    ? 0
+    : blogs.length === 1
+    ? Number(blogs[0].likes)
+    : blogs.map((blog) => blog.likes).reduce((a, b) => a + b, 0);
 };
+
+const favoriteBlog = () => {};
+
+const mostBlogs = () => {};
+
+const mostLikes = () => {};
 
 module.exports = {
   dummy,
