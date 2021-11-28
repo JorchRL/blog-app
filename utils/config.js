b30b9f3762a2fe58c1a846be78f62ad61c006323
Jorch <jorch.rl@gmail.com>
2021-11-28 14:45:07 -0600

    Setup Node test environment

--- utils/config.js
+++ utils/config.js
@@ -1,7 +1,10 @@
 require("dotenv").config();
 
 PORT = process.env.PORT;
-MONGODB_URI = process.env.MONGODB_URI;
+MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGODB_URI
+    : process.env.TEST_MONGODB_URI;
 
 module.exports = {
   PORT,
