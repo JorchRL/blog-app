5b494c204309ba15d8e18c34ab74ce31923d6906
Jorch <jorch.rl@gmail.com>
2021-11-28 18:10:40 -0600

    Refactor blogs api to use async/await

--- controllers/blogsRouter.js
+++ controllers/blogsRouter.js
@@ -1,21 +1,27 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blog");
 
-blogsRouter.get("/", (request, response, next) => {
-  console.log("get /");
-  Blog.find({})
-    .then((blogs) => response.json(blogs))
-    .catch((e) => next(e));
+// This is our blog api, handled with an express router
+
+// GET all blogs
+blogsRouter.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    response.json(blogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
-blogsRouter.post("/", (request, response, next) => {
-  console.log("post /");
-  const blog = new Blog(request.body);
-
-  blog
-    .save()
-    .then((result) => response.status(201).json(result))
-    .catch((e) => next(e));
+// POST new blog
+blogsRouter.post("/", async (request, response, next) => {
+  try {
+    const newBlog = new Blog(request.body);
+    const saveResult = await newBlog.save();
+    response.satatus(201).json(saveResult);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = blogsRouter;
