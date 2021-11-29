7e689ee3616c9d1feecd2920676de043996f1bdb
Jorch <jorch.rl@gmail.com>
2021-11-29 11:12:20 -0600

    Add template of tests for new endpoint to be implemented

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -12,68 +12,88 @@ beforeEach(async () => {
 });
 
 // GET /api/blogs
-test("returns the blogs as json", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe("Viewing existing blogs in DB", () => {
+  test("returns the blogs as json", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("returns all blogs stored in the database", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+  test("returns all blogs stored in the database", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 
-test("a returned blog has an 'id' property", async () => {
-  const response = await api.get("/api/blogs");
-  const exampleBlog = response.body[0];
-  expect(exampleBlog.id).toBeDefined();
+  test("a returned blog has an 'id' property", async () => {
+    const response = await api.get("/api/blogs");
+    const exampleBlog = response.body[0];
+    expect(exampleBlog.id).toBeDefined();
+  });
 });
 
 // POST /api/blogs
 
-test("blogs are successfully created", async () => {
-  await api.post("/api/blogs").send(helper.singleBlog);
+describe("Adding new blogs", () => {
+  test("new blogs are successfully created", async () => {
+    await api.post("/api/blogs").send(helper.singleBlog);
 
-  const blogsAtEnd = await helper.blogsInDB();
-  // added a new blog
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
-  // verify the contents of the added blog
-  expect(blogsAtEnd[blogsAtEnd.length - 1].title).toEqual(
-    helper.singleBlog.title
-  );
-  expect(blogsAtEnd[blogsAtEnd.length - 1].author).toEqual(
-    helper.singleBlog.author
-  );
-  expect(blogsAtEnd[blogsAtEnd.length - 1].url).toEqual(helper.singleBlog.url);
+    const blogsAtEnd = await helper.blogsInDB();
+    // added a new blog
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+    // verify the contents of the added blog
+    expect(blogsAtEnd[blogsAtEnd.length - 1].title).toEqual(
+      helper.singleBlog.title
+    );
+    expect(blogsAtEnd[blogsAtEnd.length - 1].author).toEqual(
+      helper.singleBlog.author
+    );
+    expect(blogsAtEnd[blogsAtEnd.length - 1].url).toEqual(
+      helper.singleBlog.url
+    );
+  });
+
+  test("if the 'likes' property is missing, sets the default value to 0", async () => {
+    const blogWithNoLikes = {
+      title: helper.singleBlog.title,
+      author: helper.singleBlog.author,
+      url: helper.singleBlog.url,
+    };
+
+    await api.post("/api/blogs").send(blogWithNoLikes);
+
+    const blogsAtEnd = await helper.blogsInDB();
+    const newBlog = blogsAtEnd[blogsAtEnd.length - 1];
+
+    expect(newBlog.likes).toBeDefined();
+    expect(newBlog.likes).toBe(0);
+  });
+
+  test("if the 'title' and 'url' properties are missing, fail with status 400 Bad Request", async () => {
+    const incompleteBlog = {
+      author: "jorch",
+      likes: 2,
+    };
+
+    await api.post("/api/blogs").send(incompleteBlog).expect(400);
+
+    const blogsAtEnd = await helper.blogsInDB();
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  });
 });
 
-test("if the 'likes' property is missing, sets the default value to 0", async () => {
-  const blogWithNoLikes = {
-    title: helper.singleBlog.title,
-    author: helper.singleBlog.author,
-    url: helper.singleBlog.url,
-  };
+// DELETE /api/blogs/id
+describe("Deleting an existing blog by id", () => {
+  test("should suceed with status code 204 if id is valid and of an existing blog", async () => {});
 
-  await api.post("/api/blogs").send(blogWithNoLikes);
-
-  const blogsAtEnd = await helper.blogsInDB();
-  const newBlog = blogsAtEnd[blogsAtEnd.length - 1];
-
-  expect(newBlog.likes).toBeDefined();
-  expect(newBlog.likes).toBe(0);
+  test("should fail with status code 400 id id is invalid or non-existing", async () => {});
 });
 
-test("if the 'title' and 'url' properties are missing, fail with status 400 Bad Request", async () => {
-  const incompleteBlog = {
-    author: "jorch",
-    likes: 2,
-  };
+// PUT /api/blogs/id
+describe("Updating an existing blog by id", () => {
+  test("should succed with status code 204 if id is valid and of an existing blog", async () => {});
 
-  await api.post("/api/blogs").send(incompleteBlog).expect(400);
-
-  const blogsAtEnd = await helper.blogsInDB();
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length);
+  test("should fail with status code 400 if id is invalid or non-existing", async () => {});
 });
 
 afterAll(() => {
