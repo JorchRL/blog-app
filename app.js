c999da313640019c61b7ad47b110767af0608a23
Jorch <jorch.rl@gmail.com>
2022-01-17 23:11:13 -0600

    Add /api/user/ router to app.js

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const config = require("./utils/config");
 const express = require("express");
 const app = express();
 const logger = require("./utils/logger");
-const blogsRouter = require("./controllers/blogsRouter");
 const middleware = require("./utils/middleware");
 const mongoose = require("mongoose");
+
+const blogsRouter = require("./controllers/blogsRouter");
+const usersRouter = require("./routes/users.routes");
 
 logger.info("connecting to", config.MONGODB_URI);
 
@@ -20,6 +22,7 @@ app.use(express.json());
 app.use(middleware.requestLogger);
 
 app.use("/api/blogs", blogsRouter);
+app.use("/api/users", usersRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
